c9c21eae675078df65b44382dd240afb59eca1a7
stkchp <s@tkch.net>
2016-07-04 20:48:32 +0900

    初めてのコミット

---
+++ htdocs/main.js
@@ -0,0 +1,249 @@
+function showDoc() {
+	hash = window.location.hash;
+	re = /^#[a-zA-Z_0-9]+$/;
+	if(re.test(hash)) {
+		$("div.show").removeClass();
+		$("div" + hash).addClass("show");
+	} else {
+		$("div.show").removeClass();
+		$("div#history").addClass("show");
+	}
+	
+	// title change
+	title = document.title;
+	document.title = title.replace(/^([^:]+).*$/, function(match, p1, offset, string) {
+		return p1 + " :: " + $("body > div.show > h2:first").text();
+	});
+
+}
+
+function check_img_safari(src) {
+	re = /^webkit-fake-url:\/\/[a-f0-9\-]+\/image\.(png|gif|tiff|jpeg|jpg)$/;
+	return re.test(src);	
+}
+
+//
+// for #paste page
+//
+var oldImg = null;
+
+function imageToCanvas(simg) {
+	
+	// store
+	oldImg = simg;
+
+	// draw canvas
+	var src = {w: 0, h: 0};
+	src.w = simg.width;
+	src.h = simg.height;
+	var dst = {w: 1500, h: 500};
+
+	var scale = parseInt($("select#pixelscale option:selected").html(), 10);
+	var tile  = parseInt($("#headertile").val(), 10);
+
+	switch (tile) {
+	case 1:
+		break;
+	case 2:
+		dst.w = parseInt($("input#tilewidth").val(), 10);
+		dst.h = parseInt($("input#tileheight").val(), 10);
+		dst.w = dst.w > 0 ? dst.w : 1;
+		dst.h = dst.h > 0 ? dst.h : 1;
+		dst.w = dst.w > 2048 ? 2048 : dst.w;
+		dst.h = dst.h > 2048 ? 2048 : dst.h;
+		break;
+	default:
+		// no tile
+		dst.w = src.w * scale > 2048 ? 2048 : src.w * scale;
+		dst.h = src.h * scale > 2048 ? 2048 : src.h * scale;
+		break;
+	}
+
+	// set canvas size
+	$("canvas#pasteimg")[0].width = dst.w;
+	$("canvas#pasteimg")[0].height = dst.h;
+
+	// prepare to write canvas
+	var canvas = $("canvas#pasteimg")[0];
+	var ctx = canvas.getContext('2d');
+
+	// not smoooooth!!
+	ctx.imageSmoothingEnabled = false;
+	ctx.msImageSmoothingEnabled = false;
+	ctx.mozImageSmoothingEnabled = false;
+
+	if(tile > 0) {
+		// fill tiling
+		for(y = 0; y < dst.h; y += src.h * scale) {
+			for (x = 0; x < dst.w; x += src.w * scale) {
+				ctx.drawImage(simg, x, y, src.w * scale, src.h * scale);
+			}
+		}
+	} else {
+		// normal
+		ctx.drawImage(simg, 0, 0, src.w * scale, src.h * scale);
+	}
+
+	// 0,0 alpha data transform
+	var p = ctx.getImageData(0, 0, 1, 1);
+	if (p.data[3]  == 0xFF) {
+		p.data[3] = 0xFE;
+	}
+	ctx.putImageData(p, 0, 0);
+
+	// show png image
+	$("img#pngimg").attr("src", canvas.toDataURL());
+
+
+	// clear canvas size
+	$("canvas#pasteimg")[0].width = 0;
+	$("canvas#pasteimg")[0].height = 0;
+
+	// delete non used img file
+	$("p#hookPaste > img").remove();
+}
+
+function urlToImage(url) {
+
+	if(url.indexOf('data:image/gif;base64,')  != 0 &&
+	   url.indexOf('data:image/png;base64,')  != 0 &&
+	   url.indexOf('data:image/tiff;base64,') != 0 &&
+	   url.indexOf('data:image/jpeg;base64,') != 0
+	   ) return;
+	var simg = new Image();
+	simg.onload = function() {
+		imageToCanvas(simg);
+	}
+	simg.src = url;
+}
+
+function getUrl(item) {
+	var blob;
+	if(typeof item.getAsFile === 'function') {
+		blob = item.getAsFile();
+	} else {
+		blob = item;
+	}
+	var reader = new FileReader();
+	reader.onloadend = function(e){
+		urlToImage(reader.result);
+	};
+
+	if (blob == null) {
+		return;
+	}
+
+	reader.readAsDataURL(blob);
+}
+
+function handlePaste(e) {
+	var file = null;
+	var cpdata = ( window.clipboardData || e.originalEvent.clipboardData );
+
+	if(cpdata.files && cpdata.files[0]) {
+		getUrl(cpdata.files[0]);
+	} else if (cpdata.items && cpdata.items[0]) {
+		getUrl(cpdata.items[0]);
+	} else {
+		// maybe firefox not support...
+		return;
+	}
+
+        // e.stopPropagation();
+	// e.preventDefault();
+
+}
+
+function handleDrop(e) {
+	e.stopPropagation();
+	e.preventDefault();
+
+	$("p#hookPaste").removeClass("drag")
+
+	var dt = (e.dataTransfer || e.originalEvent.dataTransfer);
+
+	if(dt.items != 0 && dt.items.length != 0) {
+		getUrl(dt.items[0]);
+	} else if (dt.files != null && dt.files.length != 0) {
+		getUrl(dt.files[0]);
+	} else {
+		console.log(e);
+	}
+}
+
+function handleDragLeave(e) {
+	e.stopPropagation();
+	e.preventDefault();
+	e.originalEvent.dataTransfer.dropEffect = 'none';
+	$("p#hookPaste").removeClass("drag")
+}
+
+function handleDragOver(e) {
+	e.stopPropagation();
+	e.preventDefault();
+	e.originalEvent.dataTransfer.dropEffect = 'copy';
+	$("p#hookPaste").addClass("drag")
+}
+
+
+$(document).ready(function() {
+	showDoc();
+	$(window).on('hashchange', showDoc);
+	$('nav#top a[href^="#"]').on('click', function (e) {
+        	e.preventDefault();
+            	window.location.hash = this.hash;
+		$(window).scrollTop(0);
+        });
+
+	//
+	// for #paste	
+	//
+	// paste
+	$("p#hookPaste").on("paste", handlePaste);
+
+	// drag and drop
+	$("p#hookPaste").on("dragover", handleDragOver);
+	$("p#hookPaste").on("dragleave", handleDragLeave);
+	$("p#hookPaste").on("dragend", handleDragLeave);
+	$("p#hookPaste").on("drop", handleDrop);
+
+	// no key input!!! (ctrl or meta[cmd] key is acceptable)
+	$("p#hookPaste").on('keydown', function(e) {
+		if(e.ctrlKey || e.metaKey)
+			return;
+		e.stopPropagation();
+		e.preventDefault();
+
+	});
+	// for firefox / safari hack
+	$("p#hookPaste").on('input', function(e) {
+		var img = $(this).find("img:first");
+		console.log(img);
+		if (img.length == 1) {
+			if (check_img_safari(img.attr("src"))) {
+				// safari
+				$(this).text("ここにファイルをドロップするか、カーソルを合わせて画像を貼り付けてください。(Safariはdata:形式の画像を提供していない為、貼り付けはできません)");
+			} else {
+				// firefox
+				urlToImage(img.attr("src"));
+			}
+		} else {
+			$(this).text("ここにファイルをドロップするか、カーソルを合わせて画像を貼り付けてください");
+		}
+	});
+
+	// tile size show / hide
+	$("#headertile").on('change', function() {
+		if(parseInt($(this).val(), 10) == 2) {
+			$("div#tilesize").addClass('show');
+		} else {
+			$("div#tilesize").removeClass();
+		}
+	});
+
+	// change status re image creation
+	$("#pixelscale, #headertile, #tilewidth, #tileheight").change(function() {
+		if(oldImg != null) {imageToCanvas(oldImg);}
+	});
+
+;});
---
+++ sync.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd $(dirname $0)
+CURDIR=$(pwd)
+
+
+# compress(for gzip_static)
+find ./htdocs -type f | grep -v ".gz$" | xargs gzip --best -k -f 
+
+
+
+check_size() {
+	while read line
+	do
+		BASE=`wc -c ${line}    | cut -d' ' -f1`
+		COMP=`wc -c ${line}.gz | cut -d' ' -f1`
+
+		[[ $BASE -lt $COMP ]] && rm -vf ${line}.gz
+	done
+}
+
+find ./htdocs -type f | grep -v ".gz$" | check_size
+
+
+
+
+
+# data upload(rsync...?)
+scp -r $CURDIR/htdocs/* ram.tkch.net:/var/www/ram.tkch.net/htdocs/
